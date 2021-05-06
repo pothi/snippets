d4cdba10231bf946d0cde2d55ee46f46717691e6
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2021-05-06 13:13:00 +0530

    Remove workaround for mac as it is no longer needed

--- linux/update-crontab.sh
+++ linux/update-crontab.sh
@@ -7,13 +7,13 @@
 #   - migrate to AWS CC
 
 # logging everything
-# [ ! -d ~/log ] && mkdir ~/log
-# log_file=~/log/cron.log
-# exec > >(tee -a ${log_file} )
-# exec 2> >(tee -a ${log_file} >&2)
+[ ! -d ~/log ] && mkdir ~/log
+log_file=~/log/update-cron.log
+exec > >(tee -a ${log_file} )
+exec 2> >(tee -a ${log_file} >&2)
 
-# echo; echo "Script: $0"
-# echo "Date / Time: $(date +%c)"
+echo; echo "Script: $0"
+echo "Date / Time: $(date +%c)"
 
 # today=$(date +%F)
 
@@ -33,13 +33,6 @@ if [ "x$cron_user" = "x" ]
 then
     cron_user=$(hostname)-$USER
 fi
-
-# echo "Cron User: '$cron_user'"
-
-# mac os x specific tweaks
-ssh_agent=$(pgrep ssh-agent)
-ssh_auth_sock=$(/usr/sbin/lsof -a -p $ssh_agent -U -F n | sed -n 's/^n//p')
-export SSH_AUTH_SOCK=$ssh_auth_sock
 
 cd $gitdir
 echo "Pulling changes..."
