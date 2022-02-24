13ddee53570d9c511fc439c68af03f9b66f6bfb5
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2022-02-24 10:07:41 +0530

    Detect internet before fetching the repos

--- mac/git-pull-all.sh
+++ mac/git-pull-all.sh
@@ -2,11 +2,27 @@
 
 # set -x
 
+# TODO: Check for internet and then run the rest of the code
+
+# Test for internet
+inet=
+
+while [ -z $inet ]; do
+    wget -q --spider http://g.co
+
+    if [ $? -eq 0 ]; then
+        inet="Online"
+    else
+        echo "Offline"
+        sleep 3
+    fi
+done
+
 echo "Running 'git pull' on all directories inside ~/git/ ..."
 
 for d in ~/git/*/; do
     echo; echo "Current dir: $d"
-    git -C $d pull
+    git -C $d pull -q
 done
 
 echo
