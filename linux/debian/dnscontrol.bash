c9e84f18edb7fb3d06ec0bc0c6961a291653397f
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2023-02-02 13:14:56 +0530

    Add a new script to install and/or update dnscontrol in Debian derivatives such as Ubuntu

---
+++ linux/debian/dnscontrol.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install or update DNSControl
+
+# Requirements: curl and jq
+
+latestVersion=$(curl -s "https://api.github.com/repos/StackExchange/dnscontrol/tags" | jq -r '.[0].name' | awk -Fv '{print $2}')
+
+downloadURL=https://github.com/StackExchange/dnscontrol/releases/download/v${latestVersion}/dnscontrol-${latestVersion}.amd64.deb
+
+downloadedFile=~/tmp/dnscontrol-latest.deb
+
+curl -sSLo $downloadedFile $downloadURL
+
+sudo dpkg -i $downloadedFile
+
+rm $downloadedFile
