e12b7f25d5d20b0a2f4cc1694d36175a803b127a
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2023-02-02 09:01:26 +0530

    Add a script to find locally modified repos

---
+++ git/modified-repos.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# programming env: these switches turn some bugs into errors
+# set -o errexit
+# to capture non-zero exit code in the pipeline
+set -o pipefail
+# to avoid deleting existing file accidentally
+# set -o noclobber
+# to avoid using unset variable
+set -o nounset
+
+# set -x
+
+export PATH=~/bin:~/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+
+# PATH for wget on macOS
+[ -d /opt/local/bin ] && PATH=/opt/local/bin:$PATH
+
+[ ! -d ~/log ] && mkdir ~/log
+log_file=~/log/pull-all-repos.log
+exec > >(tee -a ${log_file} )
+exec 2> >(tee -a ${log_file} >&2)
+
+echo
+echo "Script: $0"
+echo "Date / Time: $(date +%c)"
+echo
+
+# Test for internet
+inet=
+sleep_for=3
+
+while [ -z $inet ]; do
+    # \curl -s --connect-timeout 3 -o /dev/null http://1.1.1.1
+    \wget --spider -q http://g.co
+
+    if [ $? -eq 0 ]; then
+        inet="Online"
+    else
+        echo "Waiting for internet..."
+        sleep ${sleep_for}
+        [ $sleep_for -lt 60 ] && sleep_for=$((${sleep_for}*2))
+    fi
+done
+
+# echo 'Internet is up!'
+# exit
+
+gitStatus==
+
+gitStatus=$(git -C ~/.ssh status --short)
+if [ "$gitStatus" ] ; then
+    echo 'git status on ~/.ssh ...'
+    echo "$gitStatus"
+    echo
+fi
+
+echo "Running 'git status' on all directories inside ~/git/ ..."
+echo
+for repo in ~/git/*/; do
+    gitStatus=$(git -C $repo status --short)
+    if [ "$gitStatus" ] ; then
+        echo "Current dir: $repo"
+        echo "$gitStatus"
+        echo
+    fi
+done
+
+echo 'Done.'
+echo
