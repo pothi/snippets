1cb8e9bbc4e19f9415f76e71b107999b68ff4a05
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2023-02-27 14:15:23 +0530

    Improve logic and docs

--- git/modified-repos.bash
+++ git/modified-repos.bash
@@ -22,20 +22,20 @@ export PATH=~/bin:~/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 
 echo
 echo "Script: $0"
-echo "Date / Time: $(date +%c)"
+echo "Date & Time: $(date +%c)"
 echo
 
 # Test for internet
-inet=
+isUP=
 sleep_for=3
 
-while [ ! "$inet" ]; do
+while [ ! "$isUP" ]; do
     # \curl -s --connect-timeout 3 -o /dev/null http://1.1.1.1
     cmd="wget --spider -q http://g.co"
 
     if $cmd
     then
-        inet="Online"
+        isUP="Yes"
     else
         echo "Waiting for internet..."
         sleep ${sleep_for}
@@ -43,16 +43,14 @@ while [ ! "$inet" ]; do
     fi
 done
 
-# echo 'Internet is up!'
-# exit
+# echo 'Internet is up!'; exit
 
-# gitStatus=
+# Alternativly, use git status --porcelain as per https://stackoverflow.com/a/25149786/1004587
 
 gitStatus=$(git -C ~/.ssh status --short)
 if [ "$gitStatus" ] ; then
     echo 'git status on ~/.ssh ...'
-    echo "$gitStatus"
-    echo
+    echo -e "$gitStatus\n"
 fi
 
 echo "Running 'git status' on all directories inside ~/git/ ..."
@@ -60,11 +58,9 @@ echo
 for repo in ~/git/*/; do
     gitStatus=$(git -C "$repo" status --short)
     if [ "$gitStatus" ] ; then
-        echo "Current dir: $repo"
-        echo "$gitStatus"
-        echo
+        echo "Current repo: $repo"
+        echo -e "$gitStatus\n"
     fi
 done
 
-echo 'Done.'
-echo
+echo -e "All done.\n"
