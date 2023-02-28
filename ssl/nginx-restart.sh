ef9be9dc572aa741bcfb179751bbd5e1282aa5e7
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2023-02-28 08:26:15 +0530

    Use ADMIN_EMAIL for alerts if available; check for mail command

--- ssl/nginx-restart.sh
+++ ssl/nginx-restart.sh
@@ -2,11 +2,15 @@
 
 # nginx restart script for certbot
 
-# version=2.0
+# version=2.2
 
 # put it in /etc/letsencrypt/renewal-hooks/deploy/
 # make it executable (chmod +x)
 
+# version: 2.2
+#   - date: 2023-02-20
+#   - Use ADMIN_EMAIL for alerts, if CERTBOT_ADMIN_EMAIL isn't available
+#   - Check for mail command.
 # version: 2.0
 #   - date: 2023-02-17
 #   - include PATH
@@ -36,7 +40,7 @@ export PATH=~/bin:~/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 config_live_subdir=${RENEWED_LINEAGE:-""}
 
 certbot_domain=$(basename "$config_live_subdir")
-certbot_admin_email=${CERTBOT_ADMIN_EMAIL:-"root@localhost"}
+certbot_admin_email=${CERTBOT_ADMIN_EMAIL:-${ADMIN_EMAIL:-"root@localhost"}}
 ssl_utility=/root/bin/ssl-cert-check
 old_expiry_date=
 new_expiry_date=
@@ -53,5 +57,9 @@ fi
 
 new_expiry_date=$(cat "${RENEWED_LINEAGE}/fullchain.pem" | openssl x509 -noout -dates | grep notAfter | awk -F= '{print $2}' | cut -d ' ' -f 1,2,4)
 
-printf 'Certificate for the domain %s is renewed.\n\nThe old expiry date was %s.\nThe new expiry date is %s.\n\nIf you did not expect the above output, please check the logs at /var/log/letsencrypt/.\n' "$certbot_domain" "$old_expiry_date" "$new_expiry_date" \
-    | mail -s "Renewal of $certbot_domain" $certbot_admin_email
+if command -v mail >/dev/null; then
+    printf '\nCertificate for the domain %s is renewed.\n\nThe old expiry date was %s.\nThe new expiry date is %s.\n\nIf you did not expect the above output, please check the logs at /var/log/letsencrypt/.\n' "$certbot_domain" "$old_expiry_date" "$new_expiry_date" | mail -s "Renewal of $certbot_domain" $certbot_admin_email
+else
+    echo >&2 "[Warn]: 'mail' command is not found in \$PATH; Email alerts will not be sent!"
+    printf '\nCertificate for the domain %s is renewed.\n\nThe old expiry date was %s.\nThe new expiry date is %s.\n\nIf you did not expect the above output, please check the logs at /var/log/letsencrypt/.\n' "$certbot_domain" "$old_expiry_date" "$new_expiry_date"
+fi
