ef1160f443bc6c561ec64854fa49eb65ea2aa5d8
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2024-09-27 16:44:06 +0530

    Add fish config for mac os x

---
+++ misc/mac-os-x.fish
@@ -0,0 +1,11 @@
+# specific to mac OS X
+abbr -a wget curl -sSO
+
+abbr -a python python3
+abbr -a pip pip3
+
+# the following is needed only when switching from zsh
+# fish_add_path /Library/Frameworks/Python.framework/Versions/Current/bin
+# set -x PATH "/Library/Frameworks/Python.framework/Versions/Current/bin" "$PATH"
+
+
