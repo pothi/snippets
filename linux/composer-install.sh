3ee0392a1ca6d4c31ded2e105c4227a0bbdc7491
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2024-10-22 13:40:28 +0530

    Fix minor bugs

--- linux/composer-install.sh
+++ linux/composer-install.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/env bash
 
+# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+
+version=2.0
+
 # Variables
 
-install_dir=$HOME/.local/bin
+install_dir=~/.local/bin
 
 [ ! -d $install_dir ] && mkdir -p $install_dir
 
 if [ ! -f $install_dir/composer ]; then
     echo 'Installing Composer for PHP...'
-    EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
     php -r "copy('https://getcomposer.org/installer', '/tmp/composer-setup.php');"
-    ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', '/tmp/composer-setup.php');")
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', '/tmp/composer-setup.php');")"
 
-    if [ "$EXPECTED_SIGNATURE" == "$ACTUAL_SIGNATURE" ]
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
     then
-        php /tmp/composer-setup.php --quiet --install-dir=$install_dir --filename=composer
+        >&2 echo 'ERROR: Invalid installer checksum'
+        rm /tmp/composer-setup.php
+        exit 1
     fi
+
+    php /tmp/composer-setup.php --quiet --install-dir=$install_dir --filename=composer
+
     rm /tmp/composer-setup.php &> /dev/null
 
     # setup cron to self-update composer
     crontab -l | grep -qw composer
     if [ "$?" -ne "0" ]; then
         # ( crontab -l; echo; echo "# auto-update composer - nightly" ) | crontab -
-        ( crontab -l; echo '@daily $install_dir/composer self-update &> /dev/null' ) | crontab -
+        ( crontab -l; echo "@daily $install_dir/composer self-update &> /dev/null" ) | crontab -
     fi
 
     crontab -l &> /dev/null
