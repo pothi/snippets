a2be94239583ee3ed5632afad7cb611ec34cf497
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2024-12-02 08:07:57 +0530

    Add aliases for maillog and tmlog (time machine log)

--- fish/mac-os-x-common.fish
+++ fish/mac-os-x-common.fish
@@ -1,4 +1,5 @@
 # specific to mac OS X
+# functions and aliases (abbr)
 
 # replacement for wget
 # -s: silent mode to suppress progress meter or error messages.
@@ -6,3 +7,13 @@
 # -O: save as remote file name
 # -L: follow redirect
 abbr -a wget curl -sSOL
+
+# mail log
+# ref: https://apple.stackexchange.com/a/276325/24501
+abbr -a maillog "log stream --predicate  '(process == \"smtpd\") || (process == \"smtp\")' --info"
+
+# time machine log
+# ref: https://discussions.apple.com/thread/250942260
+abbr -a tmlog 'log show --predicate \'subsystem == "com.apple.TimeMachine"\' --info --last 1h | grep -F \'eMac\' | grep -Fv \'etat\' | awk -F\']\' \'{print substr($0,1,19), $NF}\''
+# additional scripts...
+# https://discussions.apple.com/thread/251491948
