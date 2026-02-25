d936f954d12e55214daf9bde4185f067da90e21d
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2026-02-25 07:36:03 +0530

    Update docs

--- fish/dnscontrol-mac.fish
+++ fish/dnscontrol-mac.fish
@@ -1,13 +1,19 @@
 #!/usr/bin/env fish
 
-set ver 2.0
+set ver 2.1
 
 # changelog
+# 2.1
+#   - date: 2026-02-25
+#   - improve docs.
 # 2.0:
 #   - date: 2025-09-08
 #   - clean up license and readme.md
 
-# Note: Currently, arch (amd64/arm64) is tested on Oracle Ampere and DO.
+# Note: Tested on...
+#   - macOS
+#   - arm64 on Oracle Ampere
+#   - amd64 on DO.
 
 # Install or update DNSControl
 
@@ -26,6 +32,13 @@ set latest_binary dnscontrol_{$latest_version}_darwin_all
 
 set download_url https://github.com/StackExchange/dnscontrol/releases/download/v$latest_version/$latest_binary.tar.gz
 
+# pre-check
+
+echo
+echo "Bin dir: $bin_dir"
+echo "Downloads dir: $downloads_dir"
+echo
+
 if test -f $bin_dir/dnscontrol
     set current_version ($bin_dir/dnscontrol version)
     echo "Current Version: $current_version"
@@ -42,14 +55,19 @@ end
 if test $current_version = $latest_version
     echo "Latest version is already installed."
     exit 2
+else
+    echo "Latest version: $latest_version"
 end
+echo
 
+echo Downloading the latest version...
 curl -jsSL -o $downloads_dir/$latest_binary.tar.gz $download_url
 if test $status -ne 0
     echo "Error downloading the latest version. Exiting prematurely."
     exit 1
 end
 
+echo Extracting the binary from the downloaded archive...
 tar xf $downloads_dir/$latest_binary.tar.gz --directory $downloads_dir/
 if test $status -ne 0
     echo "Error extracting. Exiting prematurely"
@@ -62,6 +80,8 @@ ln -fs $downloads_dir/dnscontrol-$latest_version $bin_dir/dnscontrol
 if test $status -ne 0
     echo "Exiting switching to the latest version. Exiting prematurely"
     exit 1
+else
+    echo 'The dnscontrol binary is linked to the latest version available in the download dir.'
 end
 
 # clean up
@@ -69,4 +89,6 @@ rm $downloads_dir/$latest_binary.tar.gz
 rm $downloads_dir/LICENSE
 rm $downloads_dir/README.md
 
+echo
 echo Successfully {$action}d to version: $(dnscontrol version)
+echo
