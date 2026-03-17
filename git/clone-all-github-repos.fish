890b53c47c83617e088cceb9624bfb643f987231
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2026-03-17 08:51:24 +0530

    Display execution time; other minor improvements

--- git/clone-all-github-repos.fish
+++ git/clone-all-github-repos.fish
@@ -1,18 +1,45 @@
 #!/usr/bin/env fish
 
-set ver 1.0
+set ver 1.1
 
-set -xp PATH ~/bin:~/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+# changelog
+# version: 1.1
+#   - date: 2026-03-13
+#   - minor improvements
+
+set --local --export PATH ~/bin ~/.local/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /snap/bin
+
+# echo PATH: $PATH
 
 # check for github cli
 # check for gh-clone-org extension
 
-set clone_path ~/backups/github-repos-(date +%F)
-mkdir -p $clone_path
+# set clone_path ~/backups/github-repos-(date +%Y-%m)
+set clone_path ~/projects/clone-all-github-repos/(date +%Y-%m)
+if test -d $clone_path
+    echo
+    echo Backups are taken for this month!
+    echo Check the directory $clone_path
+    echo
+    exit 0
+else
+    mkdir -p $clone_path
+end
 
 # set gh_cli ~/.local/bin/gh
 
 # $gh_cli clone-org --path $clone_path -y pothi
 # $gh_cli clone-org --path $clone_path -y -s is:private pothi
+
+echo "Date / Time: $(date +%c)"
+set time_start (date +%s)
+
 gh clone-org --path $clone_path -y pothi
 gh clone-org --path $clone_path -y -s is:private pothi
+
+echo "Date / Time: $(date +%c)"
+set time_end (date +%s)
+set runtime (math $time_end - $time_start)
+set runtime_minutes (math -s0 $runtime / 60)
+set runtime_seconds (math $runtime % 60)
+echo Execution time: $runtime_minutes minutes $runtime_seconds seconds.
