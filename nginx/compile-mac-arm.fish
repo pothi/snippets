cb41d447b63517f01a4dbe116616d48d6d676aa6
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2026-04-07 08:35:17 +0530

    Download openssl & pcre, install as per XDG structure

--- nginx/compile-mac-arm.fish
+++ nginx/compile-mac-arm.fish
@@ -1,8 +1,12 @@
 #!/usr/bin/env fish
 
-set ver 1.0
+set ver 1.1
 
 # changelog
+# 1.1:
+#   - date: 2026-04-07
+#   - Download OpenSSL, PCRE lib files.
+#   - Install nginx locally (as per XDG structure)
 # 1.0:
 #   - date: 2026-04-02
 
@@ -17,15 +21,38 @@ end
 
 # set src_dir (mktemp -d)
 set src_dir ~/projects/nginx
-set fish_trace on
 test -d $src_dir; or mkdir -p $src_dir
 set ngx_ver 1.28.3
-exit
 
 cd $src_dir
-echo Downloading nginx source...
-curl -O https://nginx.org/download/nginx-1.28.3.tar.gz
+if not test -f nginx-$ngx_ver.tar.gz
+    printf '%-72s' 'Downloading nginx source...'
+    curl -sSLO https://nginx.org/download/nginx-$ngx_ver.tar.gz
+    echo done.
+end
+printf '%-72s' 'Extracting nginx sources archive...'
 tar xf nginx-$ngx_ver.tar.gz
+echo done.
+
 cd nginx-$ngx_ver
 
+./configure \
+    --prefix=$HOME/.local/nginx \
+    --with-openssl=../openssl-3.5.5 \
+    --with-pcre=../pcre2-10.47 \
+    --sbin-path=$HOME/.local/bin/nginx \
+    --conf-path=$HOME/.local/etc/nginx/nginx.conf \
+    --error-log-path=$HOME/log/nginx/error.log \
+    --http-log-path=$HOME/log/nginx/access.log \
+    --user=pothi \
+    --group=pothi \
+    --with-http_ssl_module \
+    --with-http_v2_module \
+    --with-http_gzip_static_module \
+    --with-debug
 
+# echo Sleeping before making nginx binary...
+# sleep 2
+
+make -j$(sysctl -n hw.ncpu)   # Use all CPU cores for faster build
+make install
