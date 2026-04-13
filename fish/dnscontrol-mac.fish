8cdd7fbbf117742940b712b7016afb21c04d0067
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2026-04-13 06:47:47 +0530

    Improve variable names

--- fish/dnscontrol-mac.fish
+++ fish/dnscontrol-mac.fish
@@ -1,8 +1,11 @@
 #!/usr/bin/env fish
 
-set ver 2.3
+set ver 2.4
 
 # changelog
+# 2.4:
+#   - date: 2026-04-13
+#   - improve variable names.
 # 2.3:
 #   - date: 2026-03-11
 #   - check for jq command.
@@ -32,7 +35,7 @@ set downloads_dir ~/.local/downloads/dnscontrol
 test -d $bin_dir; or mkdir -p $bin_dir
 test -d $downloads_dir; or mkdir -p $downloads_dir
 
-set current_version 0
+set local_version 0
 set action "update"
 
 type -q jq; or begin; echo >&2 'jq command not found'; exit 1; end
@@ -56,15 +59,17 @@ end
 
 is_internet_available; or exit 1
 
-set latest_version $(curl -jsL "https://api.github.com/repos/StackExchange/dnscontrol/tags" | jq -r '.[0].name' | awk -Fv '{print $2}')
-if test -z $latest_version
+set upstream_version $(curl -jsL "https://api.github.com/repos/StackExchange/dnscontrol/tags" | jq -r '.[0].name' | awk -Fv '{print $2}')
+# alternative way
+# set upstream_version $(curl -fsSL 'https://api.github.com/repos/StackExchange/dnscontrol/releases/latest' | jq -r .tag_name | awk -Fv '{print $2}')
+if test -z $upstream_version
     echo 'Could not find the latest version from GitHub for some unknown reason.'
     echo 'Probably check the internet connection.'
     exit 1
 end
-set latest_binary dnscontrol_{$latest_version}_darwin_all
+set latest_binary dnscontrol_{$upstream_version}_darwin_all
 
-set download_url https://github.com/StackExchange/dnscontrol/releases/download/v$latest_version/$latest_binary.tar.gz
+set download_url https://github.com/StackExchange/dnscontrol/releases/download/v$upstream_version/$latest_binary.tar.gz
 
 # pre-check
 
@@ -74,28 +79,29 @@ echo "Downloads dir: $downloads_dir"
 echo
 
 if test -f $bin_dir/dnscontrol
-    set current_version ($bin_dir/dnscontrol version)
-    echo "Installed Version: $current_version"
+    set local_version ($bin_dir/dnscontrol version)
+    echo "Local Version: $local_version"
 else
     set action "install"
 end
 
 # Debugging
-# echo "Version: $latest_version"
+# echo "Version: $upstream_version"
 # echo "Binary: $latest_binary"
 # echo "Download URL: $download_url"
 # exit
 
-if test $current_version = $latest_version
-    echo "Latest version ($latest_version) is already installed."
+# string comparision as version looks like 1.0.0
+if test $local_version = $upstream_version
+    echo "Latest version ($upstream_version) is already installed."
     exit 0
 else
-    echo "Latest version: $latest_version"
+    echo "Upstream version: $upstream_version"
 end
 echo
 
 # download only if the binary is not already downloaded.
-if not test -f $downloads_dir/dnscontrol-$latest_version
+if not test -f $downloads_dir/dnscontrol-$upstream_version
     # remove any old archive
     set --local archive $downloads_dir/$latest_binary.tar.gz
     test -f $archive; and rm $archive
@@ -116,12 +122,12 @@ if not test -f $downloads_dir/dnscontrol-$latest_version
     end
     echo done.
 
-    mv $downloads_dir/dnscontrol $downloads_dir/dnscontrol-$latest_version
+    mv $downloads_dir/dnscontrol $downloads_dir/dnscontrol-$upstream_version
 end
 
-ln -fs $downloads_dir/dnscontrol-$latest_version $bin_dir/dnscontrol
+ln -fs $downloads_dir/dnscontrol-$upstream_version $bin_dir/dnscontrol
 if test $status -ne 0
-    echo >&2 "Exiting switching to the latest version. Exiting prematurely"
+    echo >&2 "Could not switch to the latest version. Exiting prematurely"
     echo
     exit 1
 else
